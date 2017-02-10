e880db417c59e9f939bac733e79592dc3a929756
Martin Clayton <martin@Martins-MacBook-Pro.local>
2017-02-10 21:39:51 +0000

    Header and footer added

---
+++ application/views/footer_view.php
@@ -0,0 +1,6 @@
+        <!-- Javascript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!--Page rendered in <strong>{elapsed_time}</strong> seconds.-->
+    </body>
+</html>
---
+++ application/views/header_view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title></title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body <?php if(isset($bodyclass)) { echo 'class="' . $bodyclass . '"'; } ?>>
